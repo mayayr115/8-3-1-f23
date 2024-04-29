85f33c38f5bb209383f421e0c79d7803a6fb79bc
mayayr115 <mayayr115@gmail.com>
2024-04-29 19:57:57 -0400

    Edited

--- 1-fellows-tracker-database/server/index.js
+++ 1-fellows-tracker-database/server/index.js
@@ -60,5 +60,5 @@ app.get('*', (req, res, next) => {
 });
 
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`listening at http://localhost:${port}`));
